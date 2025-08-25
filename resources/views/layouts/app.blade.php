906838f943beadd68f20dbb1825ceca0c34b4711
Danialdevs <dananu986@gmail.com>
2025-08-25 15:14:08 +0500

    feat: add Yandex.Metrika tracking and refactor JS files
    refactor: improve code structure and readability in JS files
    style: format and minify JS files for better performance
    chore: update database credentials in .env file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,19 @@
     @if(View::hasSection('seo'))
         @yield('seo')
     @endif
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103848275', 'ym');
 
+    ym(103848275, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/103848275" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
     @php($favicon = $generalSettings->getRealFormat('favicon'))
     <link rel="preload" as="image" href="{{$favicon}}">
     <link rel="shortcut icon" type="image/svg" href="{{$favicon}}">
